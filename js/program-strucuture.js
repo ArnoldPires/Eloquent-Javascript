4270984410ed4befa5449ea07d010afd8137f2d3
resilientcoders6 <arnold.pires92@gmail.com>
2017-07-11 14:49:02 -0400

    added more javascript buttons to the examples

--- js/program-strucuture.js
+++ js/program-strucuture.js
@@ -1,12 +1,35 @@
 //This function is an example of how to get a button to trigger a function
 
-//variables
-document.getElementById("tenex").addEventListener('click', function()
+//variables-1
+document.getElementById("varOne").addEventListener('click', function()
 {
 var ten = 10;(alert('ten * ten'));
 alert(ten * ten);
 });
 
+//variables-2
+document.getElementById("varTwo").addEventListener('click', function()
+{
+var arnoldsDebt = 140;(alert("Arnold owe's me $140!"));
+alert("Arnold pays me $35");
+alert("Now he owe's me");
+alert(arnoldsDebt = arnoldsDebt - 35);
+});
+
+//variables-3
+document.getElementById("varThree").addEventListener('click', function()
+{
+var one = 1, two = 2
+alert("one + two equals");
+alert(one + two);
+});
+
+//Keywords and Reserved words
+document.getElementById("javaKeywords").addEventListener('click', function()
+{
+var listofkeywords = [];
+var a = ["break",""]
+});
 
 //Control Flow
 document.getElementById("clickMe").addEventListener('click', function()
