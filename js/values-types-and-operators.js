0e7d070066275f936a56367ad6106d9835b4f42a
resilientcoders6 <arnold.pires92@gmail.com>
2017-07-18 18:06:04 -0400

    Created a few javascript buttons for chapter 1

---
+++ js/values-types-and-operators.js
@@ -0,0 +1,18 @@
+//The Values of Javascript
+//Numbers
+document.getElementById("numbers").addEventListener('click', function()
+{
+alert("PEMDAS = Please Excuse My Dear Aunt Sally!");
+});
+
+//Strings
+document.getElementById("string-1").addEventListener('click', function()
+{
+alert("Fix my boat with proper tape");
+alert("My horse is waving goodbye");
+});
+
+document.getElementById("string-2").addEventListener('click', function()
+{
+alert("concatenate = con + cat + e + nate");
+});
